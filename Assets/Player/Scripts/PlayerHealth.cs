66a662e64d6b31a6aae8d15bc19fb89c6530149e
Vrishab17 <chettyvrishi@gmail.com>
2025-04-17 18:14:14 +1200

    Added Player Death
    - Added Death Screen When Player Dies
    
    - Added Death Screen
    
    -Automatic Respawn
    
    -Added Font For Death Screen
    
    Added a Respawn Point

--- Assets/Player/Scripts/PlayerHealth.cs
+++ Assets/Player/Scripts/PlayerHealth.cs
@@ -1,23 +1,44 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     [Header("Health Settings")]
     public float maxHealth = 100f;
     public float currentHealth;
+    private bool isDead = false;
 
     [Header("UI")]
     public Slider healthSlider;
+    public GameObject deathScreen;
+
+    [Header("Respawn Settings")]
+    public float respawnDelay = 3f;
+    public Transform respawnPoint;
+
+    private Vector3 defaultPosition;
+    private Quaternion defaultRotation;
 
     void Start()
     {
         currentHealth = maxHealth;
         UpdateHealthUI();
+
+        if (deathScreen != null)
+            deathScreen.SetActive(false);
+
+        if (respawnPoint == null)
+        {
+            defaultPosition = transform.position;
+            defaultRotation = transform.rotation;
+        }
     }
 
     public void TakeDamage(float amount)
     {
+        if (isDead) return;
+
         currentHealth -= amount;
         currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
         UpdateHealthUI();
@@ -30,6 +51,8 @@ public class PlayerHealth : MonoBehaviour
 
     public void Heal(float amount)
     {
+        if (isDead) return;
+
         currentHealth += amount;
         currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
         UpdateHealthUI();
@@ -43,7 +66,53 @@ public class PlayerHealth : MonoBehaviour
 
     void Die()
     {
+        if (isDead) return;
+        isDead = true;
+
         Debug.Log("Player Died");
+
+        MonoBehaviour[] components = GetComponents<MonoBehaviour>();
+        foreach (var comp in components)
+        {
+            if (comp != this)
+                comp.enabled = false;
+        }
+
+        if (deathScreen != null)
+            deathScreen.SetActive(true);
+
+        Invoke("Respawn", respawnDelay);
+    }
+
+    void Respawn()
+    {
+        isDead = false;
+
+        currentHealth = maxHealth;
+        UpdateHealthUI();
+
+        if (deathScreen != null)
+            deathScreen.SetActive(false);
+
+        MonoBehaviour[] components = GetComponents<MonoBehaviour>();
+        foreach (var comp in components)
+        {
+            comp.enabled = true;
+        }
+
+        // Move to respawn point
+        if (respawnPoint != null)
+        {
+            transform.position = respawnPoint.position;
+            transform.rotation = respawnPoint.rotation;
+        }
+        else
+        {
+            transform.position = defaultPosition;
+            transform.rotation = defaultRotation;
+        }
+
+        Debug.Log("Player Respawned");
     }
 
     void OnValidate()
