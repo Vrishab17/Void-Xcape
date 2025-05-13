c011af5c136e9f3161b9e35c8928d66a4e6828a6
Vrishab17 <chettyvrishi@gmail.com>
2025-05-14 03:02:32 +1200

    Added Prompt for Door and Soun

--- Assets/Level Desgin/Door.cs
+++ Assets/Level Desgin/Door.cs
@@ -1,28 +1,47 @@
 using UnityEngine;
+using System.Collections;
 
 public class SlidingDoor : MonoBehaviour
 {
     public Transform slidingPart;
-    public Vector3 openOffset = new Vector3(0, 0, 2); 
+    public Vector3 openOffset = new Vector3(0, 0, 2);
     public float slideSpeed = 2f;
+    public float autoCloseDelay = 3f;
+
+    public GameObject interactionPrompt;
+
+    public AudioClip openSound;
+    public AudioClip closeSound;
 
     private Vector3 closedPos;
     private Vector3 openPos;
     private bool isPlayerNear = false;
     private bool isOpen = false;
-    public GameObject interactionPrompt;
+    private Coroutine autoCloseCoroutine;
+
+    private AudioSource audioSource;
 
     void Start()
     {
         closedPos = slidingPart.position;
         openPos = closedPos + openOffset;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
     {
         if (isPlayerNear && Input.GetKeyDown(KeyCode.E))
         {
-            isOpen = !isOpen;
+            isOpen = true;
+
+            if (audioSource && openSound)
+                audioSource.PlayOneShot(openSound);
+
+            if (autoCloseCoroutine != null)
+                StopCoroutine(autoCloseCoroutine);
+
+            autoCloseCoroutine = StartCoroutine(AutoCloseAfterDelay());
         }
 
         if (isOpen)
@@ -31,15 +50,22 @@ public class SlidingDoor : MonoBehaviour
             slidingPart.position = Vector3.MoveTowards(slidingPart.position, closedPos, slideSpeed * Time.deltaTime);
     }
 
+    IEnumerator AutoCloseAfterDelay()
+    {
+        yield return new WaitForSeconds(autoCloseDelay);
+        isOpen = false;
+
+        if (audioSource && closeSound)
+            audioSource.PlayOneShot(closeSound);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             isPlayerNear = true;
-
             if (interactionPrompt != null)
                 interactionPrompt.SetActive(true);
-        
         }
     }
 
@@ -48,9 +74,8 @@ public class SlidingDoor : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isPlayerNear = false;
-
             if (interactionPrompt != null)
-            interactionPrompt.SetActive(false);
+                interactionPrompt.SetActive(false);
         }
     }
 }
