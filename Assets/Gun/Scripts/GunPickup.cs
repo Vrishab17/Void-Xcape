7db6bedb4b4a79c60c8ae1930acba88e3fbe0bba
Vrishab17 <67454717+Vrishab17@users.noreply.github.com>
2025-06-10 12:55:03 +1200

    Revert "Weapons2 hotbar  aaron"

--- Assets/Gun/Scripts/GunPickup.cs
+++ Assets/Gun/Scripts/GunPickup.cs
@@ -2,24 +2,16 @@ using UnityEngine;
 
 public class GunPickup : MonoBehaviour
 {
-    public GameObject playerGun; // The gun in the scene
+    public GameObject playerGun; // The gun in the player's hands
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             if (playerGun != null)
-            {
-                playerGun.SetActive(true);
+                playerGun.SetActive(true); // Show the held gun
 
-                WeaponManager wm = other.GetComponentInChildren<WeaponManager>();
-                if (wm != null)
-                {
-                    wm.AddWeapon(playerGun);
-                }
-            }
-
-            Destroy(gameObject);
+            Destroy(gameObject); // Remove pickup model from floor
         }
     }
 }
